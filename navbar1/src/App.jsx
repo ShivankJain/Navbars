9a7edbc36f7473775466f65da6a2ba5b04c95370
shivank55 <shivankjain555@gmail.com>
2024-07-03 22:15:16 +0530

    some progress

--- navbar1/src/App.jsx
+++ navbar1/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Button from '@mui/material/Button'
+
 
 const App = () => {
   return (
     <div>
         <h1 className='bg-red-700'>Material UI expample</h1>
 
-       <Button variant='contained' color='success'>Click Me</Button>
+       
     </div>
   )
 }
