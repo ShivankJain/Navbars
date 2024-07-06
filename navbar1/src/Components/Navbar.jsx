5e8a1648955b6acf80f133f290d8f2ecfd5bf853
shivank55 <shivankjain555@gmail.com>
2024-07-06 18:37:22 +0530

    more progress

--- navbar1/src/Components/Navbar.jsx
+++ navbar1/src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='flex'> 
-         <Link to='/' className=''>Website</Link>
-         <ul className=''>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/services'>Services</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
+    <nav className='flex justify-between items-center bg-blue-950 text-white'> 
+         <Link to='/' className='text-3xl font-bold m-16'>Website</Link>
+         <ul className='flex'>
+          <li className='block p-8 mx-0 my-8 rounded-lg'><Link to='/about'>About</Link></li>
+          <li className='block p-8 mx-0 my-8'><Link to='/services'>Services</Link></li>
+          <li className='block p-8 mx-0 my-8'><Link to='/contact'>Contact</Link></li>
          </ul>
     </nav>
   )
