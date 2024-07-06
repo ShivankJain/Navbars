abde5b98ebe3286565eadb03d6258d725bb92395
shivank55 <shivankjain555@gmail.com>
2024-07-06 16:42:33 +0530

    more progress

--- navbar1/src/Components/Pages/Services.jsx
+++ navbar1/src/Components/Pages/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae in sunt et repudiandae expedita temporibus ducimus incidunt odit aspernatur perferendis quibusdam ut exercitationem unde dolores, iure, perspiciatis mollitia? Eligendi, aut.
     </div>
   )
 }
