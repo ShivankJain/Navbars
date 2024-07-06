f3f28b9eb5a6d371666d3dc21a48a998970d9a96
shivank55 <shivankjain555@gmail.com>
2024-07-06 18:57:23 +0530

    more progress

--- navbar1/src/Components/Navbar.jsx
+++ navbar1/src/Components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link,NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center bg-blue-950 text-white'> 
          <Link to='/' className='text-3xl font-bold m-16'>Website</Link>
          <ul className='flex'>
-          <li className='block p-8 mx-0 my-8 rounded-lg'><Link to='/about'>About</Link></li>
-          <li className='block p-8 mx-0 my-8'><Link to='/services'>Services</Link></li>
-          <li className='block p-8 mx-0 my-8'><Link to='/contact'>Contact</Link></li>
+          <li className='block text-xl p-8 mx-0 my-8 rounded-lg'><NavLink to='/about' className={({isActive}) =>isActive ? 'bg-blue-500' : 'bg-blue-950'}>About</NavLink></li>
+          <li className='block text-xl p-8 mx-0 my-8 rounded-lg'><NavLink to='/services' className={({isActive}) =>isActive ? 'bg-blue-500' : 'bg-blue-950'}>Services</NavLink></li>
+          <li className='block text-xl p-8 mx-0 my-8 rounded-lg'><NavLink to='/contact' className={({isActive}) =>isActive ? 'bg-blue-500' : 'bg-blue-950'}>Contact</NavLink></li>
          </ul>
     </nav>
   )
